8c885085eb9b689a3503e768730f4a1f808532a2
GabrielleFreno <gabrielle.freno@bluewin.ch>
2020-03-06 16:37:49 +0100

    Cération d'une SplashScreen

---
+++ app/src/main/java/com/android/onlineshop_castanheirofreno/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.android.onlineshop_castanheirofreno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+        //redirection vers la page principale après 3 secondes
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        new Handler().postDelayed(run, 3000);
+    }
+}

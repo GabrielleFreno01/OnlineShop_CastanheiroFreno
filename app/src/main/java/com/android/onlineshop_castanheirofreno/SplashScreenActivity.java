29284a265b07591122fc70877ea686259f9040f0
GabrielleFreno <gabrielle.freno@bluewin.ch>
2020-03-16 14:31:07 +0100

    Drawer navigation faite, encore des erreurs dans les changments de pages

---
+++ app/src/main/java/com/android/onlineshop_castanheirofreno/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.android.onlineshop_castanheirofreno;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+        //redirection vers la page principale après 2 secondes
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        new Handler().postDelayed(run, 2000);
+    }
+}
